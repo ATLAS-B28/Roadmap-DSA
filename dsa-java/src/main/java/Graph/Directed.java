9e4ffbff41342ee5223486950e0bc9021c05fdc7
ATLAS-B28 <adityacbhambere28@gmail.com>
2023-09-18 11:18:30 +0530

    graphs and revision of code

--- dsa-java/src/main/java/Graph/Directed.java
+++ dsa-java/src/main/java/Graph/Directed.java
@@ -5,21 +5,43 @@ public class Directed {
     public Directed(){
         ADL = new HashMap<>();
     }
+    private boolean vertexExists(int vertex){
+        return !ADL.containsKey(vertex);
+    }
+    private boolean edgeExists(int src,int dest){
+        List<Integer> edges = ADL.get(src);
+        return edges != null && edges.contains(dest);
+    }
+    private void addEdge(int src,int dest){
+        List<Integer> edges = ADL.get(src);
+        if(!edges.contains(dest)){
+            edges.add(dest);
+        }
+    }
     public void addEdgeAndVertex(int src, int dest){
-        ADL.computeIfAbsent(src,k->new ArrayList<>()).add(dest);
+        if(vertexExists(src)){
+            ADL.put(src,new ArrayList<>());
+        }
+        if(vertexExists(dest)){
+            ADL.put(dest,new ArrayList<>());
+        }
+        if(!edgeExists(src,dest)){
+            addEdge(src,dest);
+        }
         //k-> new ArrayList<>() == ArrayList::new
-        ADL.computeIfAbsent(dest,ArrayList::new);
+
     }
     public void showConnections(){
         for(Map.Entry<Integer,List<Integer>> entry : ADL.entrySet()){
             int vertex = entry.getKey();
             List<Integer> edges = entry.getValue();
-            System.out.println(vertex+"->"+Arrays.toString(edges.toArray()));
+            System.out.println(vertex+"--->"+Arrays.toString(edges.toArray()));
             System.out.println();
         }
     }
     public static void main(String[] args){
         Directed g = new Directed();
+        g.addEdgeAndVertex(1,2);
         g.addEdgeAndVertex(1,2);
         g.addEdgeAndVertex(1,3);
         g.addEdgeAndVertex(1,4);
