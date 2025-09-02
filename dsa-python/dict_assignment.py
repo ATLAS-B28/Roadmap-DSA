02605dba183b1536a397b488243ac0b1cb5e8573
ATLAS-B28 <adityacbhambere28@gmail.com>
2025-09-02 16:26:24 +0530

    Adding new files

--- dsa-python/dict_assignment.py
+++ dsa-python/dict_assignment.py
@@ -55,7 +55,7 @@ for key, value in exec.items():
 
 '''
 A program to invert dict (key to value and value to key) and return the dict
-'''
+
 sample = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
 print("Original Dict:-")
 for key, value in sample.items():
@@ -74,4 +74,80 @@ for key, value in sample.items():
     print(f"{key}: {value} | ", end="\n")
 print("Inverted Dict using separate dict:-")
 for key, value in inverted.items():
+    print(f"{key}: {value} | ", end="\n")
+
+Merge two dictionaries as input and merge them, if common key found add their values instead of replacing
+
+print("Merging 2 user defined dictionaries: ")
+length = int(input("Enter the length of the dict to be merged: "))
+dict1 = {}
+dict2 = {}
+for i in range(1, length + 1):
+    key = input(f"Enter key {i} for dict 1: ")
+    value = int(input(f"Enter value {i} for dict 1: "))
+    dict1[key] = value
+for i in range(1, length + 1):
+    key = input(f"Enter key {i} for dict 2: ")
+    value = int(input(f"Enter value {i} for dict 2: "))
+    dict2[key] = value
+#merging logic 
+merged = dict1.copy() #copy dict1 to merged
+for key, value in dict2.items():
+    if key in merged.keys():
+        merged[key] += value #add values if key exists, aka this is same 
+    else:
+        merged[key] = value #add key-value pair if key doesn't exist aka unique not repeated
+print("Merged Dict:-")
+for key, value in merged.items():
+    print(f"{key}: {value} | ", end="\n")
+    '''
+'''
+User defined nested dictionaries where each student has 3 subjects with their marks
+structure - {key - Stuent Name type string: value type dict: {Subject1 type string: marks type integer,....}}
+'''
+student_entry={}
+n = int(input("Enter number of students to be added: "))
+#create a student name dict and add elements in an inner dict
+for i in range(1, n + 1):
+    student_name = input(f"Enter name of student {i}: ")
+    for j in range(1, 4):
+        sub_name = input("Enter subject name: ")
+        sub_marks = int(input("Enter subject marks: "))
+        if student_name not in student_entry.keys():
+            student_entry[student_name]={f'{sub_name} {j}': sub_marks}
+        else:
+            student_entry[student_name][f'{sub_name} {j}'] = sub_marks#updating the value of marks? 
+print("Student Data Added Successfully!")
+for key, value in student_entry.items():
+    print(f"{key}: {value} | ", end="\n")
+print()
+#average of subjects for each students
+for key, value in student_entry.items():
+    total = 0
+    for k, v in value.items():
+        total += v
+    avg = total // 3
+    print(f"Average marks of {key} is {avg}")
+#find topper in each subject
+'''
+let top marks be -1 and student as "" and 
+for key, value from student entry
+then get the subject key from i - 1 to 3
+if value from sub key is not none and sub key > top marks 
+then set top marks to subkey's value and key is student name
+in the end enter the sub key as key for topper list as this is topper -> subject 1 -> student name
+'''
+toppers = {}
+for i in range(1, 4):
+    top_marks = -1
+    top_student = ""
+    #sub_key = f"Subject {i}"
+    for student_name, subjects in student_entry.items():
+        for sub_key, marks in subjects.items():
+            if sub_key.endswith(f" {i}") and marks > top_marks:
+                top_marks = marks
+                top_student = student_name
+    toppers[f"Subject {i}"] = {top_student: top_marks}
+print("Toppers in each subject:-")
+for key, value in toppers.items():
     print(f"{key}: {value} | ", end="\n")
