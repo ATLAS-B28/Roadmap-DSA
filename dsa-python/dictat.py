8b4bbffb1cc2b4ab9e93e60a658861657920f22e
ATLAS-B28 <adityacbhambere28@gmail.com>
2025-09-02 08:45:21 +0530

    Adding the assignment in final form

--- dsa-python/dictat.py
+++ dsa-python/dictat.py
@@ -1,25 +1,30 @@
 '''
 Menu driven - Enter Sales person data, create dict for each, we could
 store in another dict, update, delete and display them all this without functions 
-2nd part is to bifurgate data using NORTH, SOUTH, EAST, WEST
+2nd part is to bifurcate data using NORTH, SOUTH, EAST, WEST
 '''
 sales = {}
-regions = {'NORTH': {}, 'SOUTH': {}, 'EAST': {}, 'WEST': {}}
-print("Sales Person Management System:-")
+#regions keys should be able to have multiple sales person info 
+#like east has 2 different sales person info which then can be displayed
+
+regions = {'NORTH': [], 'SOUTH': [], 'EAST': [], 'WEST': []}
+print("\t\t\t\tSales Person Management System 🗃️:-") 
 n = int(input("Enter the number of Sales Persons to be added: "))
-if n >= 0 and n <= 100:
+if n > 0 and n <= 100:
     for i in range(n):
+        print(f"\nEnter details for Sales Person {i+1}")
         sales_person_name=input("Enter Sales Person Name: ")
         sales_person_id=int(input("Enter Sales Person ID: "))
         sales_person_sales=int(input("Enter Sales Person Sales Performance current Quarter: "))
         sales_person_target=int(input("Enter Sales Person Target current Quarter: "))
         sales_person_region=input("Enter Sales Person Region (NORTH, SOUTH, EAST, WEST): ").upper()
+        print()
         if (sales_person_id != 0 and 
             sales_person_name.isalpha() and 
-            sales_person_id >= 0 and 
+            sales_person_id > 0 and 
             sales_person_region in regions and 
-            sales_person_sales > 0 and
-            sales_person_target > 0):
+            sales_person_sales > -1 and
+            sales_person_target > -1):
 
             sales[sales_person_id]={
                 'Sales Person ID':sales_person_id,
@@ -28,18 +33,19 @@ if n >= 0 and n <= 100:
                 'Sales Person Target Current Quarter':sales_person_target,
                 'Sales Person Region':sales_person_region
             }
-print("\nSales Person Data Added Successfully!")
+print("\nSales Persons Data Added Successfully!")
 
 #while not 0 do this operations if 0 then just break and end the program
 while True:
 #functionality to keep doing operations until the user wants for that a Yes no prompt is used
- choice = input("Do you want to continue with your choice (yes/no)? ").lower()
+ choice = input("Do you want to continue with futher operations on Sales Person Data (yes/no)? ").lower()
  if choice == 'yes':     
    print("\nOperations-")
    print("1. Update Sales Person Data")
    print("2. Delete Sales Person Data")
    print("3. Display All Sales Person Data")
    print("Any number other than 1, 2, 3 to Exit")
+   print()
    option=int(input("Enter the operation number: ")) 
    if option == 1:
      print("Enter sales person ID to update")
@@ -50,7 +56,11 @@ while True:
         sales_person_sales=int(input("Enter Sales Person Sales Performance current Quarter: "))
         sales_person_target=int(input("Enter Sales Person Target current Quarter: "))
         sales_person_region=input("Enter Sales Person Region (NORTH, SOUTH, EAST, WEST): ").upper()
-        if sales_person_name.isalpha() and sales_person_id >= 0 and sales_person_region in regions and sales_person_sales > 0:
+        if (sales_person_name.isalpha() and 
+            sales_person_id > 0 and 
+            sales_person_region in regions and 
+            sales_person_sales > -1 and
+            sales_person_target > -1):
             sales[sales_person_id]={
                 'Sales Person ID':sales_person_id,
                 'Sales Person Name':sales_person_name,
@@ -60,6 +70,7 @@ while True:
             }
             print("Sales Person Data Updated Successfully!")
             print(sales)
+            print()
         else:
             print("Invalid Input")
    elif option == 2:
@@ -77,27 +88,32 @@ while True:
         print("Sales Person Data:-")
         #formatting the print statement
         for key, value in sales[sales_person_id].items():
-            print(f"{key}: {value}\t", end="")
-        print("Sales Person Data Displayed Successfully!")
+            print(f"{key}: {value} | ", end="")
+        print("\nSales Person Data Displayed Successfully!")
+        print()
      else:
         print("Sales Person Data Not Found!")
-   elif option == 4:
-     print("Exiting the program.")
+        print()
  elif choice == 'no':
   break
 #adding the user inputted region bifurcation operation like searching a sales person
-print("\nOperation to bifurcate data based on regions underway...")
+print("\n\t\t\tOperation to bifurcate data based on regions📍 underway...")
 #for key and values in sales, if value has region key present in regions 
 for key, value in sales.items():
-    if value.get('Sales Person Region') in regions.keys():
-        regions[value.get('Sales Person Region')]=value
+    region_name = value.get('Sales Person Region')
+    if region_name in regions:
+       regions[region_name].append(value)
 region = input("Enter the region to display data (NORTH, SOUTH, EAST, WEST): ").upper()
 if region in regions:
-   print(f"Sales Person Data for {region} Region:-")
+   print(f"Sales Persons Data for {region} 📍 Region:-")
+   #Total sales persons for this region
+   print(f"Total Sales Persons in {region} Region: {len(regions[region])}")   
    #formatting the print
-   for key, value in regions[region].items():
-       if key == 'Sales Person Region':
+   for person in regions[region]:
+      for key, value in person.items():
+         if key == 'Sales Person Region':
            continue
-       print(f"{key}: {value}\t", end="")
+         print(f"{key}: {value} |", end="")
+      print()
 else:
    print("Region not found please enter proper region name.")
